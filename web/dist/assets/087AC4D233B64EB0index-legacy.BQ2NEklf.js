a390ed16dcbc403fde46eccc1e2d71b56bdad34a
msean <msean@mseans-MacBook-Pro.local>
2025-11-03 14:43:16 +0700

    add dist

---
+++ web/dist/assets/087AC4D233B64EB0index-legacy.BQ2NEklf.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1762155586000 */
+System.register(["./087AC4D233B64EB0index-legacy.C_wXtOCP.js"],function(e,n){"use strict";var t,u,r,i,a,c,o,l,f,s,d;return{setters:[function(e){t=e.a7,u=e.g,r=e.c,i=e.o,a=e.f,c=e.w,o=e.a8,l=e.i,f=e.a9,s=e.v,d=e._}],execute:function(){e("default",Object.assign({name:"System"},{__name:"index",setup:function(e){var n=t();return function(e,t){var v=u("router-view");return i(),r("div",null,[a(v,null,{default:c(function(e){var t=e.Component;return[a(o,{mode:"out-in",name:"el-fade-in-linear"},{default:c(function(){return[(i(),l(f,{include:s(n).keepAliveRouters},[(i(),l(d(t)))],1032,["include"]))]}),_:2},1024)]}),_:1})])}}}))}}});
---
+++ web/dist/assets/087AC4D233B64EB0reload.CN6lCfcV.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1762155586000 */
+import{u as a,c as e,o as s}from"./087AC4D233B64EB0index.BO0Ws2rJ.js";const o=Object.assign({name:"Reload"},{__name:"reload",setup:o=>(a().go(-1),(a,o)=>(s(),e("div")))});export{o as default};
