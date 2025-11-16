5088cc61efe1901b2eaa06b41c480639e69a361e
msean <msean@mseans-MacBook-Pro.local>
2025-11-16 12:01:29 +0700

    机器人管理修改

--- server/api/bot/bot_task.go
+++ server/api/bot/bot_task.go
@@ -1,18 +1,15 @@
 package bot
 
 import (
-	
+	"github.com/gin-gonic/gin"
 	"github.com/msean/botmanager/server/global"
-    "github.com/msean/botmanager/server/model/common/response"
-    "github.com/msean/botmanager/server/model/bot"
-    botReq "github.com/msean/botmanager/server/model/bot/request"
-    "github.com/gin-gonic/gin"
-    "go.uber.org/zap"
+	"github.com/msean/botmanager/server/model/bot"
+	botReq "github.com/msean/botmanager/server/model/bot/request"
+	"github.com/msean/botmanager/server/model/common/response"
+	"go.uber.org/zap"
 )
 
-type BotTaskApi struct {}
-
-
+type BotTaskApi struct{}
 
 // CreateBotTask 创建任务列表
 // @Tags BotTask
@@ -24,8 +21,8 @@ type BotTaskApi struct {}
 // @Success 200 {object} response.Response{msg=string} "创建成功"
 // @Router /task/createBotTask [post]
 func (taskApi *BotTaskApi) CreateBotTask(c *gin.Context) {
-    // 创建业务用Context
-    ctx := c.Request.Context()
+	// 创建业务用Context
+	ctx := c.Request.Context()
 
 	var task bot.BotTask
 	err := c.ShouldBindJSON(&task)
@@ -33,13 +30,13 @@ func (taskApi *BotTaskApi) CreateBotTask(c *gin.Context) {
 		response.FailWithMessage(err.Error(), c)
 		return
 	}
-	err = taskService.CreateBotTask(ctx,&task)
+	err = taskService.CreateBotTask(ctx, &task)
 	if err != nil {
-        global.GVA_LOG.Error("创建失败!", zap.Error(err))
-		response.FailWithMessage("创建失败:" + err.Error(), c)
+		global.GVA_LOG.Error("创建失败!", zap.Error(err))
+		response.FailWithMessage("创建失败:"+err.Error(), c)
 		return
 	}
-    response.OkWithMessage("创建成功", c)
+	response.OkWithMessage("创建成功", c)
 }
 
 // DeleteBotTask 删除任务列表
@@ -52,14 +49,14 @@ func (taskApi *BotTaskApi) CreateBotTask(c *gin.Context) {
 // @Success 200 {object} response.Response{msg=string} "删除成功"
 // @Router /task/deleteBotTask [delete]
 func (taskApi *BotTaskApi) DeleteBotTask(c *gin.Context) {
-    // 创建业务用Context
-    ctx := c.Request.Context()
+	// 创建业务用Context
+	ctx := c.Request.Context()
 
 	ID := c.Query("ID")
-	err := taskService.DeleteBotTask(ctx,ID)
+	err := taskService.DeleteBotTask(ctx, ID)
 	if err != nil {
-        global.GVA_LOG.Error("删除失败!", zap.Error(err))
-		response.FailWithMessage("删除失败:" + err.Error(), c)
+		global.GVA_LOG.Error("删除失败!", zap.Error(err))
+		response.FailWithMessage("删除失败:"+err.Error(), c)
 		return
 	}
 	response.OkWithMessage("删除成功", c)
@@ -74,14 +71,14 @@ func (taskApi *BotTaskApi) DeleteBotTask(c *gin.Context) {
 // @Success 200 {object} response.Response{msg=string} "批量删除成功"
 // @Router /task/deleteBotTaskByIds [delete]
 func (taskApi *BotTaskApi) DeleteBotTaskByIds(c *gin.Context) {
-    // 创建业务用Context
-    ctx := c.Request.Context()
+	// 创建业务用Context
+	ctx := c.Request.Context()
 
 	IDs := c.QueryArray("IDs[]")
-	err := taskService.DeleteBotTaskByIds(ctx,IDs)
+	err := taskService.DeleteBotTaskByIds(ctx, IDs)
 	if err != nil {
-        global.GVA_LOG.Error("批量删除失败!", zap.Error(err))
-		response.FailWithMessage("批量删除失败:" + err.Error(), c)
+		global.GVA_LOG.Error("批量删除失败!", zap.Error(err))
+		response.FailWithMessage("批量删除失败:"+err.Error(), c)
 		return
 	}
 	response.OkWithMessage("批量删除成功", c)
@@ -97,8 +94,8 @@ func (taskApi *BotTaskApi) DeleteBotTaskByIds(c *gin.Context) {
 // @Success 200 {object} response.Response{msg=string} "更新成功"
 // @Router /task/updateBotTask [put]
 func (taskApi *BotTaskApi) UpdateBotTask(c *gin.Context) {
-    // 从ctx获取标准context进行业务行为
-    ctx := c.Request.Context()
+	// 从ctx获取标准context进行业务行为
+	ctx := c.Request.Context()
 
 	var task bot.BotTask
 	err := c.ShouldBindJSON(&task)
@@ -106,10 +103,10 @@ func (taskApi *BotTaskApi) UpdateBotTask(c *gin.Context) {
 		response.FailWithMessage(err.Error(), c)
 		return
 	}
-	err = taskService.UpdateBotTask(ctx,task)
+	err = taskService.UpdateBotTask(ctx, &task)
 	if err != nil {
-        global.GVA_LOG.Error("更新失败!", zap.Error(err))
-		response.FailWithMessage("更新失败:" + err.Error(), c)
+		global.GVA_LOG.Error("更新失败!", zap.Error(err))
+		response.FailWithMessage("更新失败:"+err.Error(), c)
 		return
 	}
 	response.OkWithMessage("更新成功", c)
@@ -125,18 +122,19 @@ func (taskApi *BotTaskApi) UpdateBotTask(c *gin.Context) {
 // @Success 200 {object} response.Response{data=bot.BotTask,msg=string} "查询成功"
 // @Router /task/findBotTask [get]
 func (taskApi *BotTaskApi) FindBotTask(c *gin.Context) {
-    // 创建业务用Context
-    ctx := c.Request.Context()
+	// 创建业务用Context
+	ctx := c.Request.Context()
 
 	ID := c.Query("ID")
-	retask, err := taskService.GetBotTask(ctx,ID)
+	retask, err := taskService.GetBotTask(ctx, ID)
 	if err != nil {
-        global.GVA_LOG.Error("查询失败!", zap.Error(err))
-		response.FailWithMessage("查询失败:" + err.Error(), c)
+		global.GVA_LOG.Error("查询失败!", zap.Error(err))
+		response.FailWithMessage("查询失败:"+err.Error(), c)
 		return
 	}
 	response.OkWithData(retask, c)
 }
+
 // GetBotTaskList 分页获取任务列表列表
 // @Tags BotTask
 // @Summary 分页获取任务列表列表
@@ -147,8 +145,8 @@ func (taskApi *BotTaskApi) FindBotTask(c *gin.Context) {
 // @Success 200 {object} response.Response{data=response.PageResult,msg=string} "获取成功"
 // @Router /task/getBotTaskList [get]
 func (taskApi *BotTaskApi) GetBotTaskList(c *gin.Context) {
-    // 创建业务用Context
-    ctx := c.Request.Context()
+	// 创建业务用Context
+	ctx := c.Request.Context()
 
 	var pageInfo botReq.BotTaskSearch
 	err := c.ShouldBindQuery(&pageInfo)
@@ -156,18 +154,18 @@ func (taskApi *BotTaskApi) GetBotTaskList(c *gin.Context) {
 		response.FailWithMessage(err.Error(), c)
 		return
 	}
-	list, total, err := taskService.GetBotTaskInfoList(ctx,pageInfo)
+	list, total, err := taskService.GetBotTaskInfoList(ctx, pageInfo)
 	if err != nil {
-	    global.GVA_LOG.Error("获取失败!", zap.Error(err))
-        response.FailWithMessage("获取失败:" + err.Error(), c)
-        return
-    }
-    response.OkWithDetailed(response.PageResult{
-        List:     list,
-        Total:    total,
-        Page:     pageInfo.Page,
-        PageSize: pageInfo.PageSize,
-    }, "获取成功", c)
+		global.GVA_LOG.Error("获取失败!", zap.Error(err))
+		response.FailWithMessage("获取失败:"+err.Error(), c)
+		return
+	}
+	response.OkWithDetailed(response.PageResult{
+		List:     list,
+		Total:    total,
+		Page:     pageInfo.Page,
+		PageSize: pageInfo.PageSize,
+	}, "获取成功", c)
 }
 
 // GetBotTaskPublic 不需要鉴权的任务列表接口
@@ -178,13 +176,13 @@ func (taskApi *BotTaskApi) GetBotTaskList(c *gin.Context) {
 // @Success 200 {object} response.Response{data=object,msg=string} "获取成功"
 // @Router /task/getBotTaskPublic [get]
 func (taskApi *BotTaskApi) GetBotTaskPublic(c *gin.Context) {
-    // 创建业务用Context
-    ctx := c.Request.Context()
+	// 创建业务用Context
+	ctx := c.Request.Context()
 
-    // 此接口不需要鉴权
-    // 示例为返回了一个固定的消息接口，一般本接口用于C端服务，需要自己实现业务逻辑
-    taskService.GetBotTaskPublic(ctx)
-    response.OkWithDetailed(gin.H{
-       "info": "不需要鉴权的任务列表接口信息",
-    }, "获取成功", c)
+	// 此接口不需要鉴权
+	// 示例为返回了一个固定的消息接口，一般本接口用于C端服务，需要自己实现业务逻辑
+	taskService.GetBotTaskPublic(ctx)
+	response.OkWithDetailed(gin.H{
+		"info": "不需要鉴权的任务列表接口信息",
+	}, "获取成功", c)
 }
