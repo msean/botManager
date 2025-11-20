b1c89caaae0d49aa1310cf2c0a45d79097caf08d
msean <msean@mseans-MacBook-Pro.local>
2025-11-20 10:45:30 +0700

    修改

--- web/dist/assets/087AC4D233B64EB0botChatGroup-legacy.C8kArOCo.js
+++
@@ -1,4 +0,0 @@
-/*! 
- Build based on gin-vue-admin 
- Time : 1763385040000 */
-System.register(["./087AC4D233B64EB0index-legacy.CvsxptUB.js"],function(t,e){"use strict";var r;return{setters:[function(t){r=t.s}],execute:function(){t("c",function(t){return r({url:"/botChatGroup/createBotChatGroup",method:"post",data:t})}),t("d",function(t){return r({url:"/botChatGroup/deleteBotChatGroup",method:"delete",params:t})}),t("a",function(t){return r({url:"/botChatGroup/deleteBotChatGroupByIds",method:"delete",params:t})}),t("u",function(t){return r({url:"/botChatGroup/updateBotChatGroup",method:"put",data:t})}),t("f",function(t){return r({url:"/botChatGroup/findBotChatGroup",method:"get",params:t})}),t("g",function(t){return r({url:"/botChatGroup/getBotChatGroupList",method:"get",params:t})})}}});
