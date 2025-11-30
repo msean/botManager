cc7658c8f249cf8de9cdf08bb2790e783870f08f
msean <msean@mseans-MacBook-Pro.local>
2025-11-30 23:06:10 +0700

    添加

--- server/service/recharge/pay.go
+++ server/service/recharge/pay.go
@@ -153,25 +153,25 @@ func reconcileAccount(botModel bot.Bot) (err error) {
 		global.GVA_LOG.Error("获取链上交易失败", zap.Error(err))
 		return
 	}
-	trxResp.Data = append(trxResp.Data, utils.TronResponseData{
-		TransactionID:  "mock_tx_1001",
-		BlockTimestamp: 1764518680000, // 2025-11-30 22:40 北京时间
-		From:           "TEST_FROM_ADDRESS",
-		To:             "TKBDsYcVgvBMFi2qmhf88JDaMPYkqH8x2E",
-		Type:           "Transfer",
-		Value:          "10076000", // 10.085 * 1e6
-		TokenInfo: struct {
-			Symbol   string `json:"symbol"`
-			Address  string `json:"address"`
-			Decimals int    `json:"decimals"`
-			Name     string `json:"name"`
-		}{
-			Symbol:   "USDT",
-			Address:  "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
-			Decimals: 6,
-			Name:     "Tether USD",
-		},
-	})
+	// trxResp.Data = append(trxResp.Data, utils.TronResponseData{
+	// 	TransactionID:  "mock_tx_1001",
+	// 	BlockTimestamp: 1764518680000, // 2025-11-30 22:40 北京时间
+	// 	From:           "TEST_FROM_ADDRESS",
+	// 	To:             "TKBDsYcVgvBMFi2qmhf88JDaMPYkqH8x2E",
+	// 	Type:           "Transfer",
+	// 	Value:          "10076000", // 10.085 * 1e6
+	// 	TokenInfo: struct {
+	// 		Symbol   string `json:"symbol"`
+	// 		Address  string `json:"address"`
+	// 		Decimals int    `json:"decimals"`
+	// 		Name     string `json:"name"`
+	// 	}{
+	// 		Symbol:   "USDT",
+	// 		Address:  "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+	// 		Decimals: 6,
+	// 		Name:     "Tether USD",
+	// 	},
+	// })
 	var orders []recharge.UserRechargeRecord
 	err = global.GVA_DB.Where("bot_id = ? AND status = 1", botID).Find(&orders).Error
 	if err != nil {
@@ -201,10 +201,10 @@ func reconcileAccount(botModel bot.Bot) (err error) {
 				}
 
 				for _, ch := range channels {
-					global.GVA_LOG.Error("HandleAdConfirm TgSend", zap.Int64("channelID", ch.ChannelID), zap.Error(err))
-					// if _, err = botHandler.TgSend(token, ch.ChannelID, medias, buttons); err != nil {
-					// 	global.GVA_LOG.Error("HandleAdConfirm TgSend", zap.Int64("channelID", ch.ChannelID), zap.Error(err))
-					// }
+					// global.GVA_LOG.Error("HandleAdConfirm TgSend", zap.Int64("channelID", ch.ChannelID), zap.Error(err))
+					if _, err = botHandler.TgSend(token, ch.ChannelID, medias, buttons); err != nil {
+						global.GVA_LOG.Error("HandleAdConfirm TgSend", zap.Int64("channelID", ch.ChannelID), zap.Error(err))
+					}
 				}
 				for i := range medias {
 					if medias[i].Type == "text" {
