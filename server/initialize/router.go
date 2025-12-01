1f0584d798d7efce5e3a7b005b3c59dd0aa246a9
msean <msean@mseans-MacBook-Pro.local>
2025-12-01 10:56:28 +0700

    fix

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/msean/botmanager/server/docs"
 	"github.com/msean/botmanager/server/global"
 	"github.com/msean/botmanager/server/middleware"
 	"github.com/msean/botmanager/server/router"
@@ -102,7 +101,7 @@ func Routers() *gin.Engine {
 	// Router.Use(middleware.Cors()) // 直接放行全部跨域请求
 	// Router.Use(middleware.CorsByRules()) // 按照配置的规则放行跨域请求
 	// global.GVA_LOG.Info("use middleware cors")
-	docs.SwaggerInfo.BasePath = global.GVA_CONFIG.System.RouterPrefix
+	// docs.SwaggerInfo.BasePath = global.GVA_CONFIG.System.RouterPrefix
 	Router.GET(global.GVA_CONFIG.System.RouterPrefix+"/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	global.GVA_LOG.Info("register swagger handler")
 	// 方便统一添加路由组前缀 多服务器上线使用
