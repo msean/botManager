bf73d9374ac2e21f4208bcd091035d52e651e742
msean <msean@mseans-MacBook-Pro.local>
2025-12-02 11:05:13 +0700

    fix

--- server/api/bot/handle/cmd/cmd.go
+++ server/api/bot/handle/cmd/cmd.go
@@ -19,11 +19,11 @@ import (
 var (
 	startCmd          = "/start"        // 开始按钮
 	AdPublishCmd      = "/publishAd"    // 广告发布
-	AdCancelCmd       = "/AdCancel"     // 取消广告发布
-	AdRcvContentCmd   = "/AdRcvContent" // 收到广告内容
-	AdConfirmCmd      = "/AdConfirm"    // 确认发布
-	NoticeRechargeCmd = "/NoticeRecharge"
-	RechargeCmd       = "/Recharge"
+	AdCancelCmd       = "/adCancel"     // 取消广告发布
+	AdRcvContentCmd   = "/adRcvContent" // 收到广告内容
+	AdConfirmCmd      = "/adConfirm"    // 确认发布
+	NoticeRechargeCmd = "/noticeRecharge"
+	RechargeCmd       = "/recharge"
 )
 
 var (
