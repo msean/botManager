94a091419245c2b6f0adf3826b873410af4fa69a
msean <msean@mseans-MacBook-Pro.local>
2025-12-16 12:29:59 +0700

    同步群聊消息

--- server/model/bot/bot_chat_group.go
+++ server/model/bot/bot_chat_group.go
@@ -12,7 +12,7 @@ type BotChatGroup struct {
 	BotID         int64  `json:"botID" form:"botID" gorm:"column:bot_id;"`                                       //机器人ID
 	ChatGroupID   int64  `json:"chatGroupID" form:"chatGroupID" gorm:"comment:群组ID;column:chat_group_id;"`       //群组ID
 	ChatGroupName string `json:"chatGroupName" form:"chatGroupName" gorm:"comment:群组ID;column:chat_group_name;"` //群组ID
-	SyncMessage   int64  `json:"syncMessage" form:"botID" gorm:"column:syncMessage;default:1"`                   // 是否需要同步消息, 1 是 2否
+	SyncMessage   int64  `json:"syncMessage" form:"botID" gorm:"column:sync_message;default:1"`                  // 是否需要同步消息, 1 是 2否
 }
 
 // TableName 机器人群组列表 BotChatGroup自定义表名 bot_chat_group
