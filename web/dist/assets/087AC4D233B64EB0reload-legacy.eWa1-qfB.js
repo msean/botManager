ee34296d2b1930e5c507c9ebf59d7edb337d86b1
msean <msean@mseans-MacBook-Pro.local>
2026-01-25 17:23:55 +0700

    fix

---
+++ web/dist/assets/087AC4D233B64EB0reload-legacy.eWa1-qfB.js
@@ -0,0 +1,4 @@
+/*! 
+ Build based on gin-vue-admin 
+ Time : 1769336313000 */
+System.register(["./087AC4D233B64EB0index-legacy.uS2VBIL3.js"],function(e,t){"use strict";var n,r,u;return{setters:[function(e){n=e.i,r=e.c,u=e.e}],execute:function(){e("default",Object.assign({name:"Reload"},{__name:"reload",setup:function(e){return n().go(-1),function(e,t){return u(),r("div")}}}))}}});
