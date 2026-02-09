50bea15e6762280c14d7d2957cc18cd7366f9d93
msean <msean@mseans-MacBook-Pro.local>
2026-02-09 14:44:53 +0700

    fix

--- server/api/bot/handle/chat_group/msg_mass/cmd.go
+++ server/api/bot/handle/chat_group/msg_mass/cmd.go
@@ -3,7 +3,6 @@ package msgmass
 import (
 	"github.com/msean/botmanager/server/global"
 	"github.com/msean/botmanager/server/model/bot"
-	"github.com/msean/botmanager/server/service/cache"
 	botapi "github.com/msean/botmanager/server/utils/bot_handler/bot_api"
 	"go.uber.org/zap"
 )
@@ -34,16 +33,16 @@ func (c *ParserChain) Handle(botModel bot.Bot, update botapi.Update) error {
 	chatGroupID := update.Message.Chat.ID
 
 	for _, parser := range c.parsers {
-		global.GVA_LOG.Debug("ParserChain Handle4", zap.Any("bot", botModel))
+		global.GVA_LOG.Debug("msgmass ParserChain Handle4", zap.Any("bot", botModel))
 		if !parser.Match(botModel, update) {
 			continue
 		}
 
-		global.GVA_LOG.Debug("ParserChain Handle5", zap.Any("parser", parser))
+		global.GVA_LOG.Debug("msgmass ParserChain Handle5", zap.Any("parser", parser))
 		// 权限感知
 		if p, ok := parser.(PermissionAware); ok {
 
-			global.GVA_LOG.Debug("ParserChain Handle6", zap.Any("parser", parser))
+			global.GVA_LOG.Debug("msgmass ParserChain Handle6", zap.Any("parser", parser))
 			var isAdminUser bool
 			isAdminUser, err := IsChatAdmin(
 				botModel.Token,
@@ -51,15 +50,15 @@ func (c *ParserChain) Handle(botModel bot.Bot, update botapi.Update) error {
 				update.Message.From.ID,
 				update.Message.From.UserName,
 			)
-			global.GVA_LOG.Debug("ParserChain Handle7", zap.Any("err", err), zap.Any("ok", ok))
+			global.GVA_LOG.Debug("msgmass ParserChain Handle7", zap.Any("err", err), zap.Any("ok", ok))
 			if err != nil || !ok {
 				return nil // 静默失败，不给回复
 			}
 
-			global.GVA_LOG.Debug("ParserChain Handle8", zap.Any("isAdminUser", isAdminUser))
+			global.GVA_LOG.Debug("msgmass ParserChain Handle8", zap.Any("isAdminUser", isAdminUser))
 			// 管理员权限（最高优先级）
 			if p.NeedAdmin() && !isAdminUser {
-				global.GVA_LOG.Info("ParserChain not isAdminUser", zap.Any("user", update.Message.Chat.UserName))
+				global.GVA_LOG.Info("msgmass ParserChain not isAdminUser", zap.Any("user", update.Message.Chat.UserName))
 				return nil
 			}
 
@@ -80,44 +79,17 @@ func Handle(botModel bot.Bot, tgMsg botapi.Update) (err error) {
 	if tgMsg.Message == nil || tgMsg.Message.Text == "" {
 		return
 	}
-	chain := NewParserChain()
+	chain := NewParserChain(&BotMassMsgHandler{})
 
 	return chain.Handle(botModel, tgMsg)
 }
 
-// 检测非admin用户是否有权限
-func HasPerMission(botModel bot.Bot, tgMsg botapi.Update) (ledgerPermission *cache.LedgerPermissionCache, has bool, permit bool, err error) {
-
-	chatGroupID := tgMsg.Message.Chat.ID
-	userID := tgMsg.Message.From.ID
-	userName := tgMsg.Message.From.UserName
-
-	ledgerPermission = cache.NewLedgerPermissionCache(botModel.BotID, chatGroupID)
-
-	if has, err = cache.CacheGetItem(ledgerPermission); err != nil {
-		global.GVA_LOG.Error("Ledger HasPerMission", zap.Error(err))
-		return
-	}
-
-	global.GVA_LOG.Debug("Ledger HasPerMission", zap.Any("has", has), zap.Error(err), zap.Any("ledgerPermission", ledgerPermission))
-	if !has {
-		return
-	}
-
-	if !ledgerPermission.HasUserPermission(userID, userName) {
-		return
-	}
-
-	permit = true
-	return
-}
-
 // 是否是admin 用户
 func IsChatAdmin(botToken string, chatID int64, userID int64, userName string) (bool, error) {
-	// 写死
-	if userID == 7449031746 || userID == 8099503790 || userName == "xmpaymo" {
-		return true, nil
-	}
+	// // 写死
+	// if userID == 7449031746 || userID == 8099503790 || userName == "xmpaymo" {
+	// 	return true, nil
+	// }
 
 	bot, err := botapi.NewBotAPI(botToken)
 	if err != nil {
