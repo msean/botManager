af20d9f5aa584621472a891f10ae2314cabe6853
msean <msean@mseans-MacBook-Pro.local>
2026-03-10 10:28:50 +0700

    fix

--- server/api/v1/bot/enter.go
+++ server/api/v1/bot/enter.go
@@ -3,11 +3,10 @@ package bot
 import "github.com/msean/botmanager/server/service"
 
 var (
-	botChatGroupService     = service.ServiceGroupApp.BotServiceGroup.BotChatGroupService
-	botBanGroupMemService   = service.ServiceGroupApp.BotServiceGroup.BotBanGroupMemService
-	taskService             = service.ServiceGroupApp.BotServiceGroup.BotTaskService
-	botChannelService       = service.ServiceGroupApp.BotServiceGroup.BotChannelService
-	botCmdConfigService     = service.ServiceGroupApp.BotServiceGroup.BotCmdConfigService
-	botMsgMassService       = service.ServiceGroupApp.BotServiceGroup.BotMsgMassService
-	botMassMsgRecordService = service.ServiceGroupApp.BotServiceGroup.BotMassMsgRecordService
+	botChatGroupService   = service.ServiceGroupApp.BotServiceGroup.BotChatGroupService
+	botBanGroupMemService = service.ServiceGroupApp.BotServiceGroup.BotBanGroupMemService
+	taskService           = service.ServiceGroupApp.BotServiceGroup.BotTaskService
+	botChannelService     = service.ServiceGroupApp.BotServiceGroup.BotChannelService
+	botCmdConfigService   = service.ServiceGroupApp.BotServiceGroup.BotCmdConfigService
+	botMsgMassService     = service.ServiceGroupApp.BotServiceGroup.BotMsgMassService
 )
